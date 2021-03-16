f712f1d8359f29d26c74089c5ac23711834335ca
Gustavo Jeuken <gjeuken@gmail.com>
2021-03-16 22:51:11 +0100

    some initial moves

--- src/Game/Deck.js
+++ src/Game/Deck.js
@@ -1,11 +1,11 @@
 
-function Card(category, number, letter, num_effect) {
+function Card(category, number, letter, num_effect, effect) {
     const card = {};
     card.category = category;
 	card.number = number;
 	card.letter = letter;
     card.num_effect = num_effect;
-    card.name = category + number.toString();
+	card.effect = effect;
     return card;
 }
 
@@ -18,9 +18,9 @@ function CreateMoneyDeck(n_players) {
 
     var n;
     for (n = 0; n < num_each; n++) {
-        let c1 = Card("g", 1, null, null);
-        let c2 = Card("g", 2, null, null);
-        let c3 = Card("g", 3, null, null);
+        let c1 = Card("g", 1, null, null, null);
+        let c2 = Card("g", 2, null, null, null);
+        let c3 = Card("g", 3, null, null, null);
         money_deck.push(c1);
         money_deck.push(c2);
         money_deck.push(c3);
@@ -33,27 +33,27 @@ function CreateCategoryDeck(n_players) {
     let category_deck = [];
     var n;
 
-	let letters = ['A','B','C','D','E','F','G','H'];
+	let letters = ['A','B','C','D','E','F','G','H', 'I'];
 
 	let l_ind = 0;
     // Add first two categories
     for (n = 0; n < 4; n++) {
-        let c1 = Card("a", "2", letters[l_ind], null);
-        let c2 = Card("b", "2", letters[l_ind], null);
+        let c1 = Card("a", 2, letters[l_ind], null, null);
+        let c2 = Card("b", 2, letters[l_ind], null, null);
         category_deck.push(c1);
         category_deck.push(c2);
 		l_ind++;
     }
     for (n = 0; n < 3; n++) {
-        let c1 = Card("a", "3", letters[l_ind], null);
-        let c2 = Card("b", "3", letters[l_ind], null);
+        let c1 = Card("a", 3, letters[l_ind], null, null);
+        let c2 = Card("b", 3, letters[l_ind], null, null);
         category_deck.push(c1);
         category_deck.push(c2);
 		l_ind++;
     }
     for (n = 0; n < 2; n++) {
-        let c1 = Card("a", "4", letters[l_ind], null);
-        let c2 = Card("b", "4", letters[l_ind], null);
+        let c1 = Card("a", 4, letters[l_ind], null, null);
+        let c2 = Card("b", 4, letters[l_ind], null, null);
         category_deck.push(c1);
         category_deck.push(c2);
 		l_ind++;
@@ -62,18 +62,18 @@ function CreateCategoryDeck(n_players) {
     // Add last three categories
 	l_ind = 0;
     for (n = 0; n < 7; n++) {
-        let c1 = Card("c", "1", letters[l_ind], null);
-        let c2 = Card("d", "1", letters[l_ind], null);
-        let c3 = Card("e", "1", letters[l_ind], null);
+        let c1 = Card("c", 1, letters[l_ind], null, null);
+        let c2 = Card("d", 1, letters[l_ind], null, null);
+        let c3 = Card("e", 1, letters[l_ind], null, null);
         category_deck.push(c1);
         category_deck.push(c2);
         category_deck.push(c3);
 		l_ind++;
     }
     for (n = 0; n < 2; n++) {
-        let c1 = Card("c", "2", letters[l_ind], null);
-        let c2 = Card("d", "2", letters[l_ind], null);
-        let c3 = Card("e", "2", letters[l_ind], null);
+        let c1 = Card("c", 2, letters[l_ind], null, null);
+        let c2 = Card("d", 2, letters[l_ind], null, null);
+        let c3 = Card("e", 2, letters[l_ind], null, null);
         category_deck.push(c1);
         category_deck.push(c2);
         category_deck.push(c3);
@@ -81,13 +81,13 @@ function CreateCategoryDeck(n_players) {
     }
 
     // Add special cards
-    let c = Card("s", "+1-1", 1);
+    let c = Card("s", null, null, 1, 'plusminus');
     category_deck.push(c);
     for (n = 0; n < 2; n++) {
-        let c1 = Card("s", "+1", null, 1);
-        let c2 = Card("s", "-1", null, 1);
-        let c3 = Card("s", "+1", null, 2);
-        let c4 = Card("s", "-1", null, 2);
+        let c1 = Card("s", null, null, 1, 'plus');
+        let c2 = Card("s", null, null, 1, 'minus');
+        let c3 = Card("s", null, null, 2, 'plus');
+        let c4 = Card("s", null, null, 2, 'minus');
         category_deck.push(c1);
         category_deck.push(c2);
         category_deck.push(c3);
@@ -103,6 +103,7 @@ export function CreateDeck(n_players) {
 
     // Combine decks and shuffle
     deck = deck.concat(category_deck);
+
     deck = deck.map((a) => ({sort: Math.random(), value: a})).sort((a, b) => a.sort - b.sort).map((a) => a.value)
 
     // Remove cards
--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,39 +1,116 @@
 import { INVALID_MOVE } from 'boardgame.io/core';
 import { CreateDeck } from './Deck.js'
 
+let dice = {
+	a: 3,
+	b: 3,
+	c: 3,
+	d: 3,
+	e: 3,
+}
+
+function InitializePlayers(n) {
+	let players = []
+	for (let i = 0; i < n; i++) {
+		players.push({
+			hand: [],
+			num_take: 0,
+			num_give: 0,
+			num_auction: 0,
+		})
+	}
+	return players
+}
+
+function SortCards(cards) {
+	cards.sort((a, b) => (a.category > b.category) ? 1 : (a.category === b.category) ? ((a.letter > b.letter) ? 1 : (a.letter === b.letter) ? ((a.number > b.number) ? 1 : -1) : -1
+) : -1 ); // Sort the cards by color and number(letter)
+	return cards
+}
+
 function DrawCardFromPile(G, ctx) {
+	if (G.active_card.length > 0) { return INVALID_MOVE }
     const card = G.deck.pop();
     G.active_card.push(card);
 }
 
+function TakeCardFromActive(G, ctx) {
+	if (G.players[ctx.currentPlayer].num_take > 0) { return INVALID_MOVE }
+	G.players[ctx.currentPlayer].num_take++;
+	const card = G.active_card.pop();
+	if (card.effect !== null) {
+		G.active_special_card.push(card);
+		ctx.events.setStage('special');
+	} else {
+		G.players[ctx.currentPlayer].hand.push(card);
+		G.players[ctx.currentPlayer].hand = SortCards(G.players[ctx.currentPlayer].hand)
+	}
+}
+
+function TakeCardFromPublic(G, ctx, index) {
+	const card = G.public_area[index];
+	G.public_area.splice(index,1);
+	if (card.effect !== null) {
+		G.active_special_card.push(card)
+		ctx.events.setStage('special');
+	} else {
+		G.players[ctx.currentPlayer].hand.push(card);
+		G.players[ctx.currentPlayer].hand = SortCards(G.players[ctx.currentPlayer].hand)
+	}
+}
+
+function CardToPublicArea(G, ctx) {
+	if (G.players[ctx.currentPlayer].num_give >= G.num_max_give) { return INVALID_MOVE }
+	G.players[ctx.currentPlayer].num_give++;
+	const card = G.active_card.pop();
+	G.public_area.push(card);
+}
+
+function CardToAuctionDeck(G, ctx) {
+	if (G.players[ctx.currentPlayer].num_auction > 0) { return INVALID_MOVE }
+	G.players[ctx.currentPlayer].num_auction++;
+	const card = G.active_card.pop();
+	G.auction_deck.push(card);
+}
+
+function ResetPlayerCounters(G, ctx) {
+	G.players[ctx.currentPlayer].num_take = 0;
+	G.players[ctx.currentPlayer].num_give = 0;
+	G.players[ctx.currentPlayer].num_auction = 0;
+}
+
 
 export const Greenhouse = {
-    name: 'Greenhouse',
-    minPlayers: 2,
+	name: 'Greenhouse',
+	minPlayers: 2,
 	maxPlayers: 4,
 
-    setup: ctx => {
-        let deck = CreateDeck(ctx.numPlayers);
-
-        return ({ dice: [3, 3, 3, 3, 3],
-         deck: deck,
-         num_allocate:  ctx.numPlayers + 1,
-         hand: Array(ctx.numPlayers).fill([]),
-         public_area: [],
-         active_card: [],
-         auction_deck: [],
-         disableUndo: true, })
-    },
+	setup: ctx => {
+		return ({ 
+			dice: dice,
+			deck: CreateDeck(ctx.numPlayers),
+			num_max_give:  ctx.numPlayers - 1,
+			public_area: [],
+			active_card: [],
+			auction_deck: [],
+			players: InitializePlayers(ctx.numPlayers),
+			active_special_card: [],
+			disableUndo: true, 
+		})
+	},
 
 	phases: {
 		gift_phase: {
 			moves: {
-				// CardToPublicArea,
-				// TakeCard,
-				// CardToAuctionPile,
+				DrawCardFromPile,
+				TakeCardFromActive,
+				TakeCardFromPublic,
+				CardToPublicArea,
+				CardToAuctionDeck,
 			},
 			next: 'auction_phase',
 	        start: true,
+			moveLimit: (G, ctx) => G.num_max_give + 2,
         },
         auction_phase: {
             next: 'end_phase',
@@ -43,17 +120,21 @@ export const Greenhouse = {
     },
 
 	turn: {
+		onBegin: (G, ctx) => ResetPlayerCounters(G, ctx),
+			// if (ctx.phase === 'giving_phase') { ctx.events.setStage('giving'); }
+			// else if (ctx.phase === 'action_phase') { ctx.events.setStage('bidding'); };
+		// },
 		stages: {
 			giving : {
 				moves: {
 					DrawCardFromPile,
-					// CardToPublicArea,
-					// TakeCard,
-					// CardToAuctionPile,
+					TakeCardFromActive,
+					CardToAuctionDeck,
+					CardToPublicArea,
 				},
 			},
 			recieving: {
-				// moves: { TakeCard }
+				// moves: { TakeCardFromPublic }
 			},
 			special: {
 				// moves: { ChangeDie }
